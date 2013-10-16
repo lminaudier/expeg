8d6d6b631b60806f97f3c233b2ddbcdad4bc7ac2
Loïc Minaudier <loic.minaudier@gmail.com>
2013-10-16 17:02:29 +0200

    Add basic parser combinators and matchers

--- lib/expeg.ex
+++ lib/expeg.ex
@@ -1,2 +1,141 @@
 defmodule Expeg do
+  def string(s) do
+    fn (input) ->
+      case String.starts_with?(input, s) do
+        true ->
+          consume(s, input)
+        _ ->
+          {:error, ""}
+      end
+    end
+  end
+
+  def assert(f) do
+    fn (input) ->
+      case f.(input) do
+        {:error, _} ->
+          {:error, ""}
+        {_, _} ->
+          {[], input}
+      end
+    end
+  end
+
+  def not(f) do
+    fn (input) ->
+      case f.(input) do
+        {:error, _} ->
+          {[], input}
+        {_, _} ->
+          {:error, ""}
+      end
+    end
+  end
+
+  def optionnal(f) do
+    fn (input) ->
+      case f.(input) do
+        {:error, _} ->
+          {[], input}
+        {_, _} ->
+          consume(String.at(input, 0), input)
+      end
+    end
+  end
+
+  def choose(f1, f2) do
+    fn (input) ->
+      case f1.(input) do
+        {:error, _} ->
+          case f2.(input) do
+            {:error, _} ->
+              {:error, ""}
+            {match, rest} ->
+              {match, rest}
+          end
+        {match, rest} ->
+          {match, rest}
+      end
+    end
+  end
+
+  def sequence(f1, f2) do
+    fn (input) ->
+      case f1.(input) do
+        {:error, _} ->
+          {:error, ""}
+        {matched1, rest1} ->
+          case f2.(rest1) do
+            {:error, _} ->
+              {:error, ""}
+            {matched2, rest2} ->
+              {matched1 <> matched2, rest2}
+          end
+      end
+    end
+  end
+
+  def anything do
+    fn (input) ->
+      case input do
+        "" ->
+          {:error, ""}
+        _ ->
+          consume(String.at(input, 0), input)
+      end
+    end
+  end
+
+  def charclass(class) do
+    fn (input) ->
+      s = String.at(input, 0)
+      {:ok, regex} = Regex.compile(class)
+      case Regex.match? regex, s do
+        true ->
+          consume(s, input)
+        _ ->
+          {:error, ""}
+      end
+    end
+  end
+
+  def one_or_more(f) do
+    fn (input) ->
+      case f.(input) do
+        {:error, _} ->
+          {:error, ""}
+        {matched, rest} ->
+          do_one_or_more(f, matched, rest)
+      end
+    end
+  end
+
+  defp do_one_or_more(f, matched, remaining) do
+    case f.(remaining) do
+      {:error, _} ->
+        {matched, remaining}
+      {next, rest} ->
+        do_one_or_more(f, matched <> next, rest)
+    end
+  end
+
+  def zero_or_more(f) do
+    fn (input) ->
+      case f.(input) do
+        {:error, _} ->
+          {[], input}
+        {matched, rest} ->
+          do_one_or_more(f, matched, rest)
+      end
+    end
+  end
+
+  def consume(s, input) do
+    {s, remaining(input, String.length(s))}
+  end
+
+  defp remaining(input, size) do
+    remaining_length = String.length(input) - size
+    String.slice(input, size, remaining_length)
+  end
 end
--- test/expeg_test.exs
+++ test/expeg_test.exs
@@ -1,7 +1,86 @@
 defmodule ExpegTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert(true)
+  def start_with_a_character(input, char) do
+    case String.starts_with?(input, char) do
+      true ->
+        Expeg.consume(char, input)
+      _ ->
+        {:error, ""}
+    end
+  end
+
+  def start_with_1(input) do
+    start_with_a_character(input, "1")
+  end
+
+  def start_with_12(input) do
+    start_with_a_character(input, "12")
+  end
+
+  def start_with_a(input) do
+    start_with_a_character(input, "a")
+  end
+
+  def followed_by_a_2(input) do
+    start_with_a_character(input, "2")
+  end
+
+  def followed_by_a_a(input) do
+    start_with_a_character(input, "a")
+  end
+
+  test "can consume strings" do
+    assert {"1234", "abcd"} == Expeg.string("1234").("1234abcd")
+    assert {:error, ""} == Expeg.string("1234").("abcd1234")
+  end
+
+  test "can consume with character classes" do
+    assert {"1", "234abcd"} == Expeg.charclass("[1-3]").("1234abcd")
+    assert {:error, ""} == Expeg.charclass("[A-Z]").("1234abcd")
+  end
+
+  test "can consume anything" do
+    assert {"1", "234abcd"} == Expeg.anything.("1234abcd")
+    assert {:error, ""} == Expeg.anything.("")
+  end
+
+  test "can consume optionnal expression" do
+    assert {"1", "234abcd"} == Expeg.optionnal(&start_with_1/1).("1234abcd")
+    assert {[], "abcd1234"} == Expeg.optionnal(&start_with_1/1).("abcd1234")
+  end
+
+  test "can consume an ordered choice between alternatives" do
+    assert {"1", "234abcd"} == Expeg.choose(&start_with_1/1, &start_with_a/1).("1234abcd")
+    assert {"a", "bcd1234"} == Expeg.choose(&start_with_1/1, &start_with_a/1).("abcd1234")
+    assert {:error, ""} == Expeg.choose(&start_with_1/1, &start_with_a/1).("zzz")
+  end
+
+  test "can consume a sequence of terminal and non terminals" do
+    assert {"12", "34abcd"} == Expeg.sequence(&start_with_1/1, &followed_by_a_2/1).("1234abcd")
+    assert {:error, ""} == Expeg.sequence(&start_with_1/1, &followed_by_a_a/1).("1234abcd")
+    assert {:error, ""} == Expeg.sequence(&start_with_1/1, &followed_by_a_2/1).("abcd1234")
+  end
+
+  test "can consume a greedy repetition with at least one match" do
+    assert {"1111", "abcd"} == Expeg.one_or_more(&start_with_1/1).("1111abcd")
+    assert {"1212", "abcd"} == Expeg.one_or_more(&start_with_12/1).("1212abcd")
+    assert {:error, ""} == Expeg.one_or_more(&start_with_12/1).("abcd1212")
+  end
+
+  test "can consume optional greedy repetition (any number of matches, including none)" do
+    assert {"1111", "abcd"} == Expeg.zero_or_more(&start_with_1/1).("1111abcd")
+    assert {"1212", "abcd"} == Expeg.zero_or_more(&start_with_12/1).("1212abcd")
+    assert {[], "abcd1212"} == Expeg.zero_or_more(&start_with_12/1).("abcd1212")
+  end
+
+  test "can make positive lookahead" do
+    assert {[], "1234abcd"} == Expeg.assert(&start_with_1/1).("1234abcd")
+    assert {:error, ""} == Expeg.assert(&start_with_1/1).("abcd1234")
+  end
+
+  test "can make negative lookahead" do
+    assert {[], "abcd1234"} == Expeg.not(&start_with_1/1).("abcd1234")
+    assert {:error, ""} == Expeg.not(&start_with_1/1).("1234abcd")
   end
 end
