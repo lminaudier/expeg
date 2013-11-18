2c2db4116ff91b914148ea79b7dc4352fb6c2f05
Loïc Minaudier <loic.minaudier@gmail.com>
2013-11-18 22:00:13 +0100

    Better integration tests separation

--- test/examples/arithmetic_test.exs
+++ test/examples/arithmetic_test.exs
@@ -1,3 +1,42 @@
+defmodule UntransformedArithmetic do
+  use Expeg
+
+  @moduledoc """
+  Implementation of Brian Ford's arithmetic grammar in it's thesis on Packrat Parsing
+
+      additive  <- multitive "+" additive / multitive
+      multitive <- primary "*" multitive / primary
+      primary   <- "(" additive ")" / numeral
+      numeral   <- [0-9]+
+  """
+  root(:additive)
+
+  rule(:numeral) do
+    one_or_more(charclass("[0-9]"))
+  end
+
+  rule(:additive) do
+    choose([sequence([tag(:left, &multitive/1),
+                      string("+"),
+                      tag(:right, &additive/1)]),
+            &multitive/1])
+  end
+
+  rule(:multitive) do
+    choose([sequence([&primary/1,
+                      string("*"),
+                      &multitive/1]),
+            &primary/1])
+  end
+
+  rule(:primary) do
+    choose([sequence([string("("),
+                      &additive/1,
+                      string(")")]),
+            &numeral/1])
+  end
+end
+
 defmodule Arithmetic do
   use Expeg
 
@@ -66,6 +105,10 @@ defmodule Integration.ArithmeticTest do
   use ExUnit.Case
 
   test "parses simple arithmetic grammar" do
+    assert [{:left, "3"}, "+", {:right, "2"}] == UntransformedArithmetic.parse("3+2")
+  end
+
+  test "can transform ast while parsing" do
     assert 5 == Arithmetic.parse("3+2")
     assert 229 == Arithmetic.parse("13+210+2*3+1")
     assert :fail == Arithmetic.parse("+2")
